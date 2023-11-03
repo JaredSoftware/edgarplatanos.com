84670bb2a855c7ea533dd30fc93fd2e80297110f
lollool2 <70750277+lollool2@users.noreply.github.com>
2023-11-03 23:59:32 +0000

    Deploying to gh-pages from @ lollool2/edgarplatanos.com@10d57ba65a010ef4d514dbe32f0fe52421d8b268 🚀

--- _nuxt/static/1699045261/dashboard/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fedgarplatanos.com\u002F_nuxt\u002Fstatic\u002F1699045261",layout:"default",error:a,serverRendered:true,routePath:"\u002Fdashboard",config:{_app:{basePath:"\u002Fedgarplatanos.com\u002F",assetsPath:"\u002Fedgarplatanos.com\u002F_nuxt\u002F",cdnURL:a}}}}(null));
