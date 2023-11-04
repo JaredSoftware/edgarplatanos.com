f6049108c46febd4985991ac04fa67a06a82b5b7
Jared Vargas <jaredwesley27@hotmail.com>
2023-11-03 19:09:52 -0500

    change button

--- pages/users.vue
+++ pages/users.vue
@@ -89,7 +89,7 @@
               <NuxtLink :to="'/user'+ '?id=' + object.id + linkForredirect">
                 <button
                   type="button"
-                  class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                  class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
                 >
                   desglosar
                 </button>
